ef0161af321d51325c42e0a34f3999474e88d77a
s1F102401225 <s1F102401225@iniad.org>
2024-06-28 14:57:19 +0900

    6/28の作業分
    主に画像関係

--- JS/Gakumasu.js
+++ JS/Gakumasu.js
@@ -1,39 +1,50 @@
 document.getElementById("btn1").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/咲季プロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/咲季全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/咲季プロフ.png";
 }
 
+
 document.getElementById("btn2").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/手毬プロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/手毬全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/手毬プロフ.png";
 }
 
 document.getElementById("btn3").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/ことねプロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/ことね全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/ことねプロフ.png";
 }
 
 document.getElementById("btn4").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/麻央プロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/麻央全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/麻央プロフ.png";
 }
 
 document.getElementById("btn5").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/リーリヤプロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/リーリヤ全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/リーリヤプロフ.png";
 }
 
 document.getElementById("btn6").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/千奈プロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/千奈全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/千奈プロフ.png";
 }
 
 document.getElementById("btn7").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/清夏プロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/清夏全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/清夏プロフ.png";
 }
 
 document.getElementById("btn8").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/広プロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/広全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/広プロフ.png";
 }
 
 document.getElementById("btn9").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/右芽プロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/右芽全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/右芽プロフ.png";
 }
 
 document.getElementById("btn10").onclick = function(){
-    document.getElementById("prof").src="Img/Gakumasu_Img/莉波プロフ.png";
+    document.getElementById("prof1").src="Img/Gakumasu_Img/莉波全身.png";
+    document.getElementById("prof2").src="Img/Gakumasu_Img/莉波プロフ.png";
 }
