19f2e069b07b38cd3c4c42706029868b715316b3
s1F102401225 <s1F102401225@iniad.org>
2024-06-28 13:06:01 +0900

    JS取り入れ

---
+++ JS/Gakumasu.js
@@ -0,0 +1,39 @@
+document.getElementById("btn1").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/咲季プロフ.png";
+}
+
+document.getElementById("btn2").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/手毬プロフ.png";
+}
+
+document.getElementById("btn3").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/ことねプロフ.png";
+}
+
+document.getElementById("btn4").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/麻央プロフ.png";
+}
+
+document.getElementById("btn5").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/リーリヤプロフ.png";
+}
+
+document.getElementById("btn6").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/千奈プロフ.png";
+}
+
+document.getElementById("btn7").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/清夏プロフ.png";
+}
+
+document.getElementById("btn8").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/広プロフ.png";
+}
+
+document.getElementById("btn9").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/右芽プロフ.png";
+}
+
+document.getElementById("btn10").onclick = function(){
+    document.getElementById("prof").src="Img/Gakumasu_Img/莉波プロフ.png";
+}
